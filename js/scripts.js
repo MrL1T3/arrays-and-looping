351bea54ae197feb9322e9512f6040fb0e841b90
Emil Anton <emilbstn@gmail.com>
2022-11-19 09:10:12 -0800

    add -1 operator within for loop to fix extra element in countArray

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function specialCounter(number) {
   let countArray = [1]
   let numberCounter = 2;
   let indexCounter = 0;
-  for (let i = 0; i < number; i++) {
+  for (let i = 0; i < number-1; i++) {
     if (indexCounter < number) {
       countArray.push(numberCounter);
       indexCounter ++;
