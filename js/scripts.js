5f6727a514168029e4a9010d06826287bf4be5ad
Emil Anton <emilbstn@gmail.com>
2022-11-19 09:30:33 -0800

    fix issues with counting loops due to misunderstanding prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 //business logic
 
 function specialCounter(number) {
-  let countArray = [1]
+  let countArray = [0]
+  let finalArray = [0];
   let numberCounter = 2;
   let indexCounter = 0;
-  for (let i = 0; i < number-1; i++) {
+  for (let i = 0; i < number; i++) {
     if (indexCounter < number) {
       countArray.push(numberCounter);
       indexCounter ++;
       numberCounter ++;
     }
   }
+
 }
